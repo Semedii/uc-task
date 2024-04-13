1ccd55917d043b230654435c9723c0e64bf4f5b1
semedii <abdisamed2023@hotmail.com>
2024-04-13 15:29:55 +0300

    improvs

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,38 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uniqcast_task/screens/homepage.dart';
 import 'package:uniqcast_task/screens/login_screens.dart';
 import 'package:uniqcast_task/utils/token.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
+    return const ProviderScope(
       child: MaterialApp(
-        home: const AuthChecker(),
+        home: AuthChecker(),
       ),
     );
   }
 }
 
 class AuthChecker extends ConsumerWidget {
-  const AuthChecker({Key? key}) : super(key: key);
+  const AuthChecker({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tokenFuture = ref.watch(tokenProvider.future);
-    
+
     return FutureBuilder<String?>(
       future: tokenFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           final String? token = snapshot.data;
           if (token != null) {
-            return HomePage();
+            return const HomePage();
           } else {
             return const LoginScreen();
           }
