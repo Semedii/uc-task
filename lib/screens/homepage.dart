d901597e56facb1461ea1601a6cbf5322ae37259
semedii <abdisamed2023@hotmail.com>
2024-04-13 14:38:02 +0300

    improved Homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,57 +11,95 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     return Scaffold(
       drawer: CustomDrawer(),
       appBar: AppBar(
         title: const Text('Packages'),
       ),
-      body: _buildBody(ref),
+      body: _mapStateToWidget(ref),
     );
   }
 
-  Widget _buildBody(WidgetRef ref) {
+  Widget _getPackageList(List<Package>? packages) {
+    return ListView.builder(
+      itemCount: packages?.length ?? 0,
+      itemBuilder: (context, index) {
+        final package = packages?[index];
+        final bool isDisabled = package?.purchasedDate == null;
+        return _getListItem(package, isDisabled, context, packages);
+      },
+    );
+  }
+
+  ListTile _getListItem(
+    Package? package,
+    bool isDisabled,
+    BuildContext context,
+    List<Package>? packages,
+  ) {
+    return ListTile(
+      title: _getListItemTitle(package, isDisabled),
+      subtitle: _getListItemSubtitle(package, isDisabled),
+      trailing: _getListItemTrailing(package, isDisabled),
+      onTap: () => _onListItemTapped(context, packages!, isDisabled),
+    );
+  }
+
+  Text _getListItemTitle(Package? package, bool isDisabled) {
+    return Text(
+      "Package ID: ${package?.id.toString()}",
+      style: _getListItemTitleStyle(isDisabled),
+    );
+  }
+
+  TextStyle _getListItemTitleStyle(bool isDisabled) {
+    return TextStyle(
+      fontWeight: isDisabled ? FontWeight.normal : FontWeight.bold,
+      color: isDisabled ? Colors.grey : Colors.black,
+    );
+  }
+
+  Text _getListItemSubtitle(Package? package, bool isDisabled) {
+    return isDisabled
+        ? const Text('Not purchased yet')
+        : Text('Purchased date: ${package?.purchasedDate}');
+  }
+
+  Widget? _getListItemTrailing(Package? package, bool isDisabled) {
+    return isDisabled
+        ? ElevatedButton(onPressed: () {}, child: const Text("Buy now"))
+        : null;
+  }
+
+  Widget _mapStateToWidget(WidgetRef ref) {
     final packageState = ref.watch(packageNotifierProvider);
-    if(packageState is PackageInitialState){
+    if (packageState is PackageInitialState) {
       ref.read(packageNotifierProvider.notifier).fetchUserPackages();
     }
     if (packageState is PackageLoadingState) {
       return const Center(child: CircularProgressIndicator());
     }
     if (packageState is PackageLoadedState) {
-      return _buildPackageList(packageState.packages);
+      return _getPackageList(packageState.packages);
     } else {
       return const Center(child: Text('Unknown state'));
     }
   }
 
-  Widget _buildPackageList(List<Package>? packages) {
-    return ListView.builder(
-      itemCount: packages?.length ?? 0,
-      itemBuilder: (context, index) {
-        final package = packages?[index];
-        final bool isDisabled = package?.purchasedDate == null;
-
-        return ListTile(
-          title: Text(
-            "Package ID: ${package?.id.toString()}",
-            style: TextStyle(
-              fontWeight: isDisabled ? FontWeight.normal : FontWeight.bold,
-              color: isDisabled ? Colors.grey : Colors.black,
-            ),
-          ),
-          subtitle: isDisabled
-              ? const Text('Not purchased yet')
-              : Text('Purchased date: ${package?.purchasedDate}'),
-          onTap: isDisabled
-              ? null
-              : () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> ChannelPage(packages: packages!)));
-                },
-        );
-      },
-    );
+  void _onListItemTapped(
+    BuildContext context,
+    List<Package> packages,
+    bool isDisabled,
+  ) {
+    isDisabled
+        ? null
+        : () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChannelPage(packages: packages),
+              ),
+            );
+          };
   }
-  
 }
