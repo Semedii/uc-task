bfe68aedbee12379c4b7d12955a3a5508fef9797
semedii <abdisamed2023@hotmail.com>
2024-04-13 14:47:44 +0300

    improved channel page

--- lib/screens/channel_page.dart
+++ lib/screens/channel_page.dart
@@ -26,18 +26,18 @@ class ChannelPage extends ConsumerWidget {
       ),
       body: Container(
         color: Colors.black,
-        child: _buildBody(ref),
+        child: _mapStateToWidget(ref),
       ),
     );
   }
 
-  Widget _buildBody(WidgetRef ref) {
+  Widget _mapStateToWidget(WidgetRef ref) {
     final channelState = ref.watch(channelNotifierProvider);
 
     if (channelState is ChannelInitialState) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
         ref.read(channelNotifierProvider.notifier).fetchPackageChannels(
-              getPackageIds(packages),
+              _getPackageIds(packages),
             );
       });
       return const Center(child: CircularProgressIndicator());
@@ -48,64 +48,90 @@ class ChannelPage extends ConsumerWidget {
     }
 
     if (channelState is ChannelLoadedState) {
-      return _buildChannelList(channelState.channels);
+      return _getChannelList(channelState.channels);
     } else {
       return const Center(child: Text('Unknown state'));
     }
   }
 
-  Widget _buildChannelList(List<Channel>? channels) {
+  Widget _getChannelList(List<Channel>? channels) {
     return ListView.builder(
       itemCount: channels?.length ?? 0,
       itemBuilder: (context, index) {
         final channel = channels?[index];
 
-        return Container(
-          decoration: const BoxDecoration(
-            border: Border(
-              bottom: BorderSide(color: Colors.grey),
-            ),
-          ),
-          child: ListTile(
-            contentPadding: const EdgeInsets.all(16.0),
-            leading: Container(
-              width: MediaQuery.of(context).size.width*.3,
-              decoration: const BoxDecoration(
-                border: Border(
-                  right: BorderSide(color: Colors.grey),
-                ),
-              ),
-              child: Image.network(
-                "https://office-new-dev.uniqcast.com:12802/api/client/v1/${channel?.ouid}/channels/${channel?.id}/logos/NORMAL?accessKey=WkVjNWNscFhORDBLCg==",
-                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  } else {
-                    return const CircularProgressIndicator(color: Colors.white); 
-                  }
-                },
-                errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
-                  return Image.asset('assets/logo/logo.png'); 
-                },
-              ),
-            ),
-            title: Text(
-              channel?.name ?? '',
-              style: const TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const VidePlayerScreen()),
-              );
-            },
-          ),
-        );
+        return _getChannelContainer(context, channel);
       },
     );
   }
 
-  List<int> getPackageIds(List<Package> packages) {
+  Container _getChannelContainer(BuildContext context, Channel? channel) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: Colors.grey),
+        ),
+      ),
+      child: _getChannelInfo(context, channel),
+    );
+  }
+
+  ListTile _getChannelInfo(BuildContext context, Channel? channel) {
+    return ListTile(
+      contentPadding: const EdgeInsets.all(16.0),
+      leading: _getChannelLogo(context, channel),
+      title: _getChannelTitle(channel),
+      onTap: () => _onChannelTapped(context),
+    );
+  }
+
+  Text _getChannelTitle(Channel? channel) {
+    return Text(
+      channel?.name ?? '',
+      style: const TextStyle(color: Colors.white),
+    );
+  }
+
+  Container _getChannelLogo(BuildContext context, Channel? channel) {
+    return Container(
+      width: MediaQuery.of(context).size.width * .3,
+      decoration: _getChannelLogoDecoration(),
+      child: _getChannelLogoImage(channel),
+    );
+  }
+
+  Image _getChannelLogoImage(Channel? channel) {
+    return Image.network(
+      "https://office-new-dev.uniqcast.com:12802/api/client/v1/${channel?.ouid}/channels/${channel?.id}/logos/NORMAL?accessKey=WkVjNWNscFhORDBLCg==",
+      loadingBuilder: (BuildContext context, Widget child,
+          ImageChunkEvent? loadingProgress) {
+        if (loadingProgress == null) {
+          return child;
+        } else {
+          return const CircularProgressIndicator(color: Colors.white);
+        }
+      },
+      errorBuilder:
+          (BuildContext context, Object error, StackTrace? stackTrace) {
+        return Image.asset('assets/logo/logo.png');
+      },
+    );
+  }
+
+  BoxDecoration _getChannelLogoDecoration() {
+    return const BoxDecoration(
+      border: Border(
+        right: BorderSide(color: Colors.grey),
+      ),
+    );
+  }
+
+  List<int> _getPackageIds(List<Package> packages) {
     return packages.map((package) => package.id).toList();
+  }
+
+  void _onChannelTapped(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => const VidePlayerScreen()));
   }
 }
